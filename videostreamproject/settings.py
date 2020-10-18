f285151c8ae06ed7ae02ecf88f3e9767eb3c2a3c
minh hoang nguyen <nhminh0701@gmail.com>
2020-10-18 12:28:05 +0700

    Add host to settings.py

--- videostreamproject/settings.py
+++ videostreamproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ep(aq193j%-$pr8ces@8mw%zw-uxnbez@wo&3+7b!@wdw26ki&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
